d546ce8a8a6efc07c9affa6225e4af2f6765eb76
mpollachek <mpollachek81@gmail.com>
2022-10-27 14:52:41 -0400

    re-init git

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose')
+const StrikeEmailsModel = require('./models/StrikeEmails');
+const newsletterEmailsModel = require('./models/NewsletterEmails');
+const { mongoPassword, mongoString } = require('./config');
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+
+//const getInvolvedRouter = require('./routes/getInvolvedRouter')
+const mongoConnection = `mongodb+srv://mpollachek81:${mongoPassword}@awu0.${mongoString}.mongodb.net/allWorkersUnionDB?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoConnection);
+
+//app.use('/GetInvolvedEmails', getInvolvedRouter);
+
+app.get("/getEmails", (req, res) => {
+  StrikeEmailsModel.countDocuments({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/addEmail", async (req, res) => {
+  const r = req.body.values
+  console.log("full req: " + JSON.stringify(r))
+  if (r.strike) {
+    const strikeEmail= new StrikeEmailsModel(r);
+    await strikeEmail.save();
+  } 
+
+  if (r.newsletter) {
+    const newsletterEmail= new newsletterEmailsModel(r);
+    await newsletterEmail.save();
+  } 
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(r);
+})
+
+app.listen(3001, () => {
+  console.log("Server is Running - booyah");
+});
